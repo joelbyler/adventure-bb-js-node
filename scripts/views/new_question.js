f4eed45b3112a377a6db7ae13c20d3e48df2f0ee
Joel Byler <joelbyler@gmail.com>
2013-10-25 14:19:40 -0400

    show / hide secitons of screen and template use

--- scripts/views/new_question.js
+++ scripts/views/new_question.js
@@ -1,14 +1,27 @@
 App.Views.NewQuestion = Backbone.View.extend({
  
-   initialize: function(options) {
-       App.currentUser.on("change", this.render, this);
-       this.render();
-   },
+  events: {
+    "click #addItem": "addItem"
+  },
+
+  initialize: function(options) {
+    App.currentUser.on("change", this.render, this);
+    this.render();
+  },
  
-   render: function() {
-       this.$("#asker").text(App.currentUser.get("userName"));
-       this.$el.toggleClass("hidden", !App.currentUser.isLoggedIn());
-       return this;
-   }
+  render: function() {
+    this.$("#asker").text(App.currentUser.get("userName"));
+    this.$el.toggleClass("hidden", !App.currentUser.isLoggedIn());
+    return this;
+  },
+
+  addItem: function() {
+    var question = {
+      text: this.$("#newQuestion").val(),
+      userName: App.currentUser.get("userName")
+    };
+    this.model.add(new App.Models.Question(question));
+    this.$("#newQuestion").val('')
+  }  
  
 });
