c3c13f8eb7f8b3958db33076c7dec3d6aff94da8
Joel Byler <joelbyler@gmail.com>
2013-10-25 14:34:05 -0400

    show vote count

--- scripts/views/question.js
+++ scripts/views/question.js
@@ -1,11 +1,24 @@
 App.Views.Question = Backbone.View.extend({
-   tagName: 'li',
-   className: 'question',
-   template: _.template($("#question-template").html()),
+  tagName: 'li',
+  className: 'question',
+  template: _.template($("#question-template").html()),
+  events: {
+    "click .voteup": "upVote",
+    "click .votedown": "downVote"
+  },
+  render: function() {
+    var data = this.model.toJSON();
+    data.tally = this.model.voteTally();
+       
+    this.$el.html(this.template(data));
+    return this;
+  },
  
-   render: function() {
-       this.$el.html(this.template(this.model.toJSON()));
-       return this;
-   }
+  upVote: function() { this.setVote("up"); },
+  downVote: function() { this.setVote("down"); },
  
+  setVote: function(voteType) {
+    this.model.vote(App.currentUser.get('userName'), voteType);
+    this.render();
+  }
 });
